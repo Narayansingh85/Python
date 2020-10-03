50468cdd8ef4bb04e9371658bc4e13a390deaed4
Narayansingh85 <37082413+Narayansingh85@users.noreply.github.com>
2020-10-03 21:48:05 +0530

    Signed-off-by: Narayansingh85 <37082413+Narayansingh85@users.noreply.github.com>

---
+++ using for loop.py
@@ -0,0 +1,19 @@
+shopping_list= ["bread","banana","eggs","milk","Potato"]
+Shoppin_it=print("""Shopping items are as follows
+Bread
+Banana
+Eggs
+Milk
+Potato""")
+item_to_find=input("Enter a Shopping Item you would like to find:").lower()
+print(item_to_find)
+found_at = None
+for item in range(len(shopping_list)):
+	if shopping_list[item]==item_to_find:
+		found_at=item
+		break
+
+if found_at is not None:
+	print("{} is at {}".format(item_to_find,found_at))
+else:
+    print("{} is not in the Menu".format(item_to_find))
