fd3e79fa8c4af44979cc890f75a3e7e1feb7ccde
Narayansingh85 <37082413+Narayansingh85@users.noreply.github.com>
2020-10-08 16:10:02 +0530

    Improved banner using keywords

--- banner.py
+++ banner.py
@@ -1,8 +1,7 @@
-def banner_text(text):
-    screen_width = 80
+def banner_text(text=" ", screen_width=80):
     if len(text) > screen_width - 4:
-        print("EEK!!")
-        print("THE TEXT IS TOO LONG TO FIT IN THE SPECIFIED WIDTH")
+        raise ValueError("String {0} is larger then specified width {1}"
+                         .format(text, screen_width))
 
     if text == "*":
         print("*" * screen_width)
@@ -12,20 +11,14 @@ def banner_text(text):
         print(output_string)
 
 
-banner_text("*")
-banner_text("Always look on the bright side of life...")
-banner_text("If life seems jolly rotten,")
-banner_text("There's something you've forgotten!")
-banner_text("And that's to laugh and smile and dance and sing,")
-banner_text(" ")
-banner_text("When you're feeling in the dumps,")
-banner_text("Don't be silly chumps,")
-banner_text("Just purse your lips and whistle - that's the thing!")
-banner_text("And... always look on the bright side of life...")
-banner_text("*")
-
-result = banner_text("Nothing is returned")
-print(result)
-
-numbers = [4, 2, 7, 5, 8, 3, 9, 6, 1]
-print(numbers.sort())
+banner_text("*", 60)
+banner_text("Always look on the bright side of life...", 60)
+banner_text("If life seems jolly rotten,", 60)
+banner_text("There's something you've forgotten!", 60)
+banner_text("And that's to laugh and smile and dance and sing,", 60)
+banner_text(screen_width=60)
+banner_text("When you're feeling in the dumps,", 60)
+banner_text("Don't be silly chumps,", 60)
+banner_text("Just purse your lips and whistle - that's the thing!", 60)
+banner_text("And... always look on the bright side of life...", 60)
+banner_text("*", 60)
