f7ca11b55f83ca2f4f118ca67e16446dab46b696
Narayansingh85 <37082413+Narayansingh85@users.noreply.github.com>
2020-10-31 23:48:59 +0530

    NEW OOPS CONCEPT

---
+++ my_file1.py
@@ -0,0 +1,5 @@
+#openning a file
+file = open("C:/Users/LoNe1412/Desktop/my_file.txt",'r')
+for line in file:
+    print(line)
+file.close()
---
+++ pythonic_way_to_read_file.py
@@ -0,0 +1,4 @@
+#pythonic way to open a file
+with open("C:/Users/LoNe1412/Desktop/my_file.txt",'r') as file1:
+    for line in file1:
+        print(line)
